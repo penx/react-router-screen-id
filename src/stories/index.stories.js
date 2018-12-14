08034f2b78cdd4893a7bfa7b6c12ca7e8a863564
Alasdair McLeay <penx@users.noreply.github.com>
2018-12-14 14:49:19 +0000

    allow dev dependencies on stories, tests, fixtures (#5)
    * allow dev dependencies on stories, tests, fixtures
    
    * fix import

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+// import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Welcome } from '@storybook/react/demo';
 
-import Example from '../src';
+import Example from '..';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
